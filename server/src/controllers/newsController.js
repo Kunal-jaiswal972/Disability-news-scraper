f199f1565c9e82f314fdf948c2f2be502ee77b87
kunal <kishujaiswal3@gmail.com>
2023-12-19 15:13:36 +0530

    routes categorised

--- server/src/controllers/newsController.js
+++ server/src/controllers/newsController.js
@@ -1,27 +1,54 @@
 import { scrapeNews } from "../lib/scrapeNews.js";
 import { CacheSchema } from "../models/cacheSchema.js";
 
-export const getNews = async (req, res) => {
-  const { page = 1, newsPerPage = 10 } = req.query;
-
-  const url = `https://timesofindia.indiatimes.com/topic/disability/news/${page}`;
+const getNews = async (url, page) => {
+  const fullUrl = `${url}/${page}`;
 
   const existingNews = await CacheSchema.findOne({
-    url,
+    url: fullUrl,
   });
 
-  //implement time function
+  // Implement time function
   if (existingNews) {
-    console.log(`Data for ${url} already exists in the database.`);
-    return res.status(200).json(existingNews);
+    console.log(`Data for ${fullUrl} already exists in the database.`);
+    return existingNews;
   }
 
-  const scrapedNews = await scrapeNews(url);
+  const scrapedNews = await scrapeNews(fullUrl);
 
   await CacheSchema.create({
-    url,
+    url: fullUrl,
     news: scrapedNews,
   });
 
-  return res.status(200).json({ news: scrapedNews });
+  return { url: fullUrl, news: scrapedNews };
+};
+
+export const getGeneralNews = async (req, res) => {
+  const { page = 1, newsPerPage = 10 } = req.query;
+  const url = "https://timesofindia.indiatimes.com/topic/disability/news";
+
+  const generalNews = await getNews(url, page);
+
+  return res.status(200).json(generalNews);
+};
+
+export const getEducationNews = async (req, res) => {
+  const { page = 1, newsPerPage = 10 } = req.query;
+  const url =
+    "https://timesofindia.indiatimes.com/topic/education-for-disabled-child-in-india";
+
+  const educationNews = await getNews(url, page);
+
+  return res.status(200).json(educationNews);
+};
+
+export const getBenefitsNews = async (req, res) => {
+  const { page = 1, newsPerPage = 10 } = req.query;
+  const url =
+    "https://timesofindia.indiatimes.com/topic/scholarship-for-disabled-students";
+
+  const benefitNews = await getNews(url, page);
+
+  return res.status(200).json(benefitNews);
 };
